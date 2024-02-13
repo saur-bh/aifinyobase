d21cf587e6ff78b5683650dce029bd0f29a869ce
Saurabh Verma <100rabh@Saurabhs-MacBook-Pro.local>
2024-02-13 19:25:13 +0100

    added Reporting option

--- cypress.config.js
+++ cypress.config.js
@@ -5,13 +5,12 @@ module.exports = defineConfig({
 	reporter: 'cypress-mochawesome-reporter',
 	reporterOptions: {
 		charts: true,
-		reportPageTitle: 'Automation Test Resport for Wikipedia.',
+		reportPageTitle: 'Automation Test Resport for Aifinyo.',
 		embeddedScreenshots: true,
 		inlineAssets: true,
-		saveAllAttempts: false,
-		defaultCommandTimeout : 100000,
-		
+		saveAllAttempts: false,	
 	},
+	defaultCommandTimeout : 100000,
 	retries: {
 		runMode: 1,
 		openMode: 1,
