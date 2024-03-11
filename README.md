07d566e55a30e946ad2ef11a99e1b7abd63b5360
Saurabh-home <saurabh-home@mb23.intern>
2024-03-11 16:52:35 +0100

    adde

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#  Aifinyo's frontend application testing using cypress -RunBook
-The test was conducted on web browser ( Chrome V111.0.5563.147 , Edge V105 and Electron V105)
+#  Aifinyo's frontend / API  End-to-End testing using cypress -RunBook
+The test was conducted on web browser
 ## Pre-Condition 
 Install node.js and npm on the system
 * Windows https://phoenixnap.com/kb/install-node-js-npm-on-windows
@@ -11,35 +11,31 @@ Install node.js and npm on the system
 - cypress
 - reporter mochawsome
 
-
 ## Demo
-
-TDB
+<<>>
 
 ## Setup
 * Navigate to desire location in your workstation and clone the project.
-* Type command `git clone https://github.com/saur-bh/pflegia-challenge.git` on the terminal. 
+* Type command `git clone https://github.com/` on the terminal. 
 * Open the folder in your favourite editor or VsCode.
 * Install dependencies by running `npm install`
 
 ## Running Tests
 
-* To run all  or  individual tests via GUI, run `npm run cy:open`
-* To run test in different configuration from command prompt, type `npm run cy:run:chrome | cy:run:edge |cy.run:electron`
+* To run all  or  individual tests via GUI, run  `npm run cy:dresden`
+* To run test in different configuration from command prompt, type `npm run `
 * To run test in CI use following link [github] 
 
 ## Creation of New Test
 Our framework is based on the Page Object Model design pattern. 
-- If required create paage, create new file in /e2e-frontend/cypress/support/pages i.e. login.js which will extend BasePage.js. 
-- Create new folder if not present under cypress/e2e/modulename for module which test has to be written.
-- If module in present , create testfile with .cy.js extension  i.e./e2e-frontend/cypress/e2e/1_Invoices/[filename].cy.js
-
-## Viewing the Results HeadlessMode 
- - Once test is run using `npm run cy:run:chrome`, result will be store in cypress/reports which is html file.
-  ![artifcat](./resource/result.PNG)
- - Execution video can be located in cypress/videos.
+- Create new project in cypress/e2e/[projectname] i.e. cypress/e2e/billomat-frontend if alreayd not there.
+- All the feature of the project will be in seperate folder i.e for Invoice Feature we will have folder as : cypress/e2e/billomat-frontend/1_Invoices 
+- Two files one for the UI and other for the API will be in this folder. i.e.  and API : cypress/e2e/billomat-frontend/1_Invoices/InvoiceAPI.cy.js
+- Any experiment or playground place is cypress/e2e/billomat-frontend/developement
+- /fixture folder will be having all the test data for different envoirnment cypress/fixtures/billomat-frontend-prod-data/prod-data.json 
+- /support folder will have all the locators and pages cypress/support/locators/billomat-frontend/clientPage.json pages cypress/support/pages/CommonItemPage.js
+- If you have to create new page make sure to extend base Page 
 ## Enhancement
   * Publish report via gitpages or on slack channel 
-  * Excution via CI pipeline github action
-  * Automation bot to execute testSuite from slack or any other medium. 
+  * from automation to manual case creation.
 
---
+++ cypress/e2e/billomat-frontend/1_Invoices/InvoiceAPI.cy.js
@@ -0,0 +1,103 @@
+describe('GraphQL API mock data', () => {
+    it('Get all import account', () => {
+        cy.intercept({
+            method: 'POST',
+            url: 'https://supergraph.billodev.net/graphql',
+          }, (request) => {
+            console.log("dkjhdhdhkdh")
+            // Check if this is the request you want to intercept
+            if (request.includes('importedBankAccounts')) {
+
+                request.reply((res) => {
+                // Modify the response data here
+                res.body.data.importedBankAccounts.forEach(account => {
+                  account.balance.available = '20000';
+                });
+              });
+            }
+          });
+          
+          // Then make the request
+          cy.request({
+            method: 'POST',
+            url: 'https://supergraph.billodev.net/graphql',
+            headers: {
+              "X-BillomatApiKey": "dresden",
+              Bauthentication: "5Xipb2/VaQF8KUO1iRTDH3nb4/jVAbP1Qr3L9WObZT59J6yA5ncMWZarnRLXV918335hhQCJKm61nEbvhU2Yge0Kpo6AYnLEDjBPkiL9115rPLrcBDY3Xc2hCJVURY5MfaxeE1OkttS7Ln9bAkQIHJTlTQ==",
+              authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijg3ZGE0MTc2MzQ5MzRhNzJhN2U2MzY1OGVkYmQyZDUwMTljNWVjYWQ0ODk2MjhmMWJmNWYzNjlhMjMwNTQxZGNlZjY4NGVlM2NiYjQzYzE1In0.eyJhdWQiOiIxIiwianRpIjoiODdkYTQxNzYzNDkzNGE3MmE3ZTYzNjU4ZWRiZDJkNTAxOWM1ZWNhZDQ4OTYyOGYxYmY1ZjM2OWEyMzA1NDFkY2VmNjg0ZWUzY2JiNDNjMTUiLCJpYXQiOjE3MDkyOTYwNTAsIm5iZiI6MTcwOTI5NjA1MCwiZXhwIjoxNzA5Mjk5NjUwLCJzdWIiOiJ1c2VyLTI0NDYiLCJzY29wZXMiOlsiYXJ0aWNsZXM6ZGVsZXRlIiwiY2xpZW50czpkZWxldGUiLCJzdXBwbGllcnM6ZGVsZXRlIiwib2ZmZXJzOmRlbGV0ZSIsImNvbmZpcm1hdGlvbnM6ZGVsZXRlIiwiaW52b2ljZXM6ZGVsZXRlIiwiY3JlZGl0X25vdGVzOmRlbGV0ZSIsImRlbGl2ZXJ5X25vdGVzOmRlbGV0ZSIsInJlbWluZGVyczpkZWxldGUiLCJpbmNvbWluZ3M6ZGVsZXRlIiwibGV0dGVyczpkZWxldGUiLCJzZXR0aW5nczpteV9hY2NvdW50OndyaXRlIiwic2V0dGluZ3M6ZG9jdW1lbnRzOndyaXRlIiwic2V0dGluZ3M6Y29uZmlndXJhdGlvbjp3cml0ZSIsInNldHRpbmdzOmFkbWluaXN0cmF0aW9uOndyaXRlIiwic2V0dGluZ3M6YWRkb25zOndyaXRlIiwic2V0dGluZ3M6bXlfYWRkb25zOndyaXRlIiwibmV0X2luY29tZV9zdGF0ZW1lbnQ6ZGVsZXRlIiwiYWNjb3VudGluZ19leHBvcnQ6d3JpdGUiLCJiYW5raW5nOndyaXRlIl0sImZlYXR1cmVzIjpbIm5vdGlmaWNhdGlvbnMiLCJmaWdvIiwiZm9udHMiLCJyb2xlcyIsImdpbmkiLCJhY2NvdW50aW5nX2V4cG9ydCIsImFjY291bnRhbnRfYWNjZXNzIiwiYmFua2luZyIsInZhdC1yZXR1cm4iXSwidXNlciI6eyJpZCI6MjQ0NiwiYWNjb3VudF9pZCI6MjQ1LCJmaXJzdF9uYW1lIjoiU2F1cmFiaCIsImJpbGxvbWF0X2lkIjoiZHJlc2RlbiJ9fQ.rMh8V70xJfAef69d2Qoe5KsBbzWsmWZ4JHlNg2k7z1rVg0PANaxu_fI2x8vQ-Q72U7p6lnVPyaozOl2e3IsZPW5uFygtMDw7Rvs0_aljrQHFRUmcsFCvcfTtAyez5GskMixTUF3S1737QvYFcSH19PdIBsqz9iBsd4lJSd0fupkfxZzGs3KIIojJfiIUnheRD7Ix7hF-5wI-3N-REZLM7QX5wgxZPe5vECbmmLiDQsKA0XyCYZY52ZWI0aksLeK9A8_u99m6ZHzxnsxodnb6pnU7CMXQYeF-IemtRbujXcRfk5T4r1rx89uuMvp0ByWsvJFEXtESHFguDbeogj2RsHKWaSIRIs3eDkyOUbc5w6z5qt-UMiEIaJdYm2Xwj45BgjsjUDfKvR7OzKTcnR6HsKREU11SbDf4m5mX5Wg9Yeuhsp0kbSMzlzWq9kxBQwxSvGj9RGMb0hg224O8NztqeWu8jPedwMtpk55pkWubnfmS5pYWArnrfLSkz9ujHQ58Vlp0wAxYZagYaG86FjMAPURtRXP8pErD1HPzdEZwBLkix0FOjeiiMt6JOlBBWX6UdxpvvJ7nxNRGMhSiwdrjOJ8TlzSiGwDNe__aZ8Sz0chpGW_EXUXNHDN_BVin0oE77LynLYRLZmdIWXEJdt9oIci83ljv-Bz58niDN6iuXdU",
+            },
+            body: {
+              query: `
+              query importedBankAccounts {
+                importedBankAccounts: myBankAccounts(showOnlyOnBoarded: true) {
+                  accountType
+                  customInitials: acronym
+                  active
+                  customName: alias
+                  balance {
+                    available
+                    balanceAmount
+                    bankAccountId
+                    createdAt
+                    currency
+                    date
+                    deletedAt
+                    id
+                    limit
+                    monthlyBalance
+                    updatedAt
+                    __typename
+                  }
+                  bankName
+                  bic
+                  country
+                  createdAt
+                  deletedAt
+                  accountName: description
+                  expiredAt
+                  fintecBankAccountId
+                  fintecBankConnectionId
+                  firstSignificantBalanceDate
+                  holder
+                  iban
+                  id
+                  jointAccount
+                  lastSyncAt
+                  customColor: preferredColor
+                  syncCount
+                  transactionPossible
+                  updatedAt
+                  user {
+                    createdAt
+                    deletedAt
+                    fintecBankUserId
+                    id
+                    principalId
+                    isAutoBookingEnabled
+                    updatedAt
+                    __typename
+                  }
+                  userId
+                  imported
+                  __typename
+                }
+              }       
+              `
+            }
+          }).then(res => {
+            
+            expect(res.status).to.eq(200)
+            cy.log("dkjhdhdhkdh")
+            cy.log(res.body.data.importedBankAccounts[2].balance.available)//.to.eq('20000')
+          
+        
+          });
+          
+      
+      
+      
+      
+    });
+;
+
+});
